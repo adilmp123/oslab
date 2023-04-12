39e414113f942d47e6c5e02c14f944a6e86d7787
adilmp123 <87594500+adilmp123@users.noreply.github.com>
2023-04-12 16:13:30 +0530

    s

--- cycle3/lab/bestfit.c
+++ cycle3/lab/bestfit.c
@@ -47,27 +47,27 @@ void bestfit(int block[], int m, int process[], int n)
 }
 int main()
 {
-    // int m,n;
-    // printf("Enter the no of blocks: ");
-    // scanf("%d",&m);
-    // int block[m];
-    // printf("Enter size of %d blocks: ");
-    // for(int i=0;i<m;i++)
-    // {
-    //     scanf("%d",block+i);
-    // }
-    // printf("Enter the no of processes: ");
-    // scanf("%d",&n);
-    // int process[n];
-    // printf("Enter size of %d processes: ");
-    // for(int i=0;i<n;i++)
-    // {
-    //     scanf("%d",process+i);
-    // }
+    int m,n;
+    printf("Enter the no of blocks: ");
+    scanf("%d",&m);
+    int block[m];
+    printf("Enter size of %d blocks: ");
+    for(int i=0;i<m;i++)
+    {
+        scanf("%d",block+i);
+    }
+    printf("Enter the no of processes: ");
+    scanf("%d",&n);
+    int process[n];
+    printf("Enter size of %d processes: ");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",process+i);
+    } 
 
-    int block[]={100,500,200,300,600};
-    int process[]={212, 417, 112, 426};
-    int m=5,n=4;
-    bestfit(block,m,process,n);
+    // int block[]={100,500,200,300,600};
+    // int process[]={212, 417, 112, 426};
+    // int m=5,n=4;
+    // bestfit(block,m,process,n);
 
 }
